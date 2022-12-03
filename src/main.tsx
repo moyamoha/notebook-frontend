0c6626d479e399ff808ee637425595e0d83dd437
Yahya Salimi <yahya.salimi@eduhouse.fi>
2022-12-03 09:39:47 +0200

    add persist to state and background color to notelist

--- src/main.tsx
+++ src/main.tsx
@@ -2,10 +2,11 @@ import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import store from './state/store';
+import store, { persistor } from './state/store';
 import App from './App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
 
 axios.defaults.baseURL = 'https://notebookapp.onrender.com';
 
@@ -23,7 +24,9 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <PersistGate loading={null} persistor={persistor}>
+          <App></App>
+        </PersistGate>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>,
--- src/state/store.ts
+++ src/state/store.ts
@@ -1,16 +1,29 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import dataReducer from './slices/data.slice';
 import noteReducer from './slices/note.slice';
 import uiReducer from './slices/ui.slice';
 import userReducer from './slices/user.slice';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+};
+
+const reducer = combineReducers({
+  data: dataReducer,
+  note: noteReducer,
+  ui: uiReducer,
+  user: userReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
 const store = configureStore({
-  reducer: {
-    data: dataReducer,
-    note: noteReducer,
-    ui: uiReducer,
-    user: userReducer,
-  },
+  reducer: persistedReducer,
 });
 
 export default store;
+
+export const persistor = persistStore(store);
