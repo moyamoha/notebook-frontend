c3fb23892685138fd505121c2391a6d254b8df12
Yahya Salimi <yahya.salimi@eduhouse.fi>
2024-11-28 19:36:14 +0200

    Changes on 2024-11-28 19:36:14.341019

--- src/components/navbar/AccountSettingsBtn.tsx
+++ src/components/navbar/AccountSettingsBtn.tsx
@@ -1,15 +1,7 @@
-import { useNavigate } from 'react-router';
-import { useDispatch } from 'react-redux';
-import { AiOutlineSetting } from 'react-icons/ai';
 import { RiAccountCircleLine } from 'react-icons/ri';
-import { useAppSelector } from '../../state/hooks';
 import { AUTH_URL, SITE_URL } from '../../utils/constants';
 
 export default function AccountSettingsBtn() {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const activeNav = useAppSelector((s) => s.ui.activeNav);
-
   const handleClick = () => {
     const token = window.localStorage.getItem('accessToken');
     window.location.href = `${AUTH_URL}/?redirect=${SITE_URL}&token=${token}`;
